f40c503904443fa94c34178db887b18cd1ac95f8
Dimas1402 <anugerahdimas469@gmail.com>
2020-08-08 16:23:50 +0700

    update passwoed

--- src/views/Settings/components/Password/Password.js
+++ src/views/Settings/components/Password/Password.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/styles';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/styles";
 import {
   Card,
   CardHeader,
@@ -9,45 +9,66 @@ import {
   CardActions,
   Divider,
   Button,
-  TextField
-} from '@material-ui/core';
+  TextField,
+} from "@material-ui/core";
+import axios from "axios";
+import { apiDashManage } from "../../../../api/api";
+import Swal from "sweetalert2";
 
 const useStyles = makeStyles(() => ({
-  root: {}
+  root: {},
 }));
 
-const Password = props => {
+const Password = (props) => {
   const { className, ...rest } = props;
 
   const classes = useStyles();
 
   const [values, setValues] = useState({
-    password: '',
-    confirm: ''
+    password: "",
+    confirm_password: "",
   });
+  const [loading, setLoading] = useState(false);
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     setValues({
       ...values,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
+  const handleConfirmPassword = () => {
+    setLoading(true);
+    axios({
+      method: "put",
+      url: `${apiDashManage + "update-password"}`,
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+      data: values,
+    })
+      .then((res) => {
+        Swal.fire("Update Password Success", "", "success");
+        setLoading(false);
+      })
+      .catch((err) => {
+        Swal.fire({
+          icon: "error",
+          title: "Please fill in correctly and check your internet connection",
+          text: "",
+        });
+        setLoading(false);
+      });
+  };
   return (
-    <Card
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-      <form>
-        <CardHeader
-          subheader="Update password"
-          title="Password"
-        />
+    <Card {...rest} className={clsx(classes.root, className)}>
+      <form onSubmit={handleConfirmPassword}>
+        <CardHeader subheader="Update password" title="Password" />
         <Divider />
         <CardContent>
           <TextField
             fullWidth
-            label="Password"
+            label="Old Password"
             name="password"
             onChange={handleChange}
             type="password"
@@ -56,22 +77,23 @@ const Password = props => {
           />
           <TextField
             fullWidth
-            label="Confirm password"
-            name="confirm"
+            label="New Password"
+            name="confirm_password"
             onChange={handleChange}
-            style={{ marginTop: '1rem' }}
+            style={{ marginTop: "1rem" }}
             type="password"
-            value={values.confirm}
+            value={values.confirm_password}
             variant="outlined"
           />
         </CardContent>
         <Divider />
         <CardActions>
           <Button
+            onClick={handleConfirmPassword}
             color="primary"
             variant="outlined"
           >
-            Update
+            {loading ? "loading..." : "Update Password"}
           </Button>
         </CardActions>
       </form>
@@ -80,7 +102,7 @@ const Password = props => {
 };
 
 Password.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
 };
 
 export default Password;
