e080876a258773050c4e689ebd57c14fc1516f92
usamahbass <basalamahusamah7@gmail.com>
2020-08-29 14:09:24 +0700

    gitignore not working

--- src/views/Category/Category.js
+++ src/views/Category/Category.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useContext } from "react";
 import { makeStyles } from "@material-ui/styles";
+import { CategoryContext } from "../../context/categoryContext";
 import MaterialTable from "material-table";
 import axios from "axios";
-import { CategoryContext } from "../../context/categoryContext";
 import LoadingOverlay from "react-loading-overlay";
 import Swal from "sweetalert2";
 
